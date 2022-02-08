c6417990bd16c762825a1848917ba205fd9e9649
jasonholloway125 <jasonholloway125@gmail.com>
2022-02-08 10:20:10 +0800

    Test Commit

--- Learning/helloWorld.cpp
+++ Learning/helloWorld.cpp
@@ -9,7 +9,7 @@ int main()
     // TODO: update the detail @jasonholloway125
 
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
+    int what = 0;
     for (const string& word : msg)
     {
         cout << word << " ";
